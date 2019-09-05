901f362333a236f8c381764aa76fa50fc380b96b
mirannaalina <v_mxa@yahoo.com>
2019-09-05 19:21:21 +0300

    materie

--- src/main/java/Materie.java
+++ src/main/java/Materie.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Materie {
+    String nume;
+    String prescurtare;
+    String detalii;
+    Profesor profesori;
+
+    public Materie(String nume, String prescurtare, String detalii, Profesor profesori){
+        this.nume = nume;
+        this.prescurtare = prescurtare;
+        this.detalii = detalii;
+        this.profesori = profesori;
+
+    }
 }

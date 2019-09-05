69876710004af32747c8ca701595d51273c18fd0
Geta Ureche <getairina@yahoo.com>
2019-09-05 19:34:43 +0300

    clasa Mesage

--- src/main/java/model/Message.java
+++ src/main/java/model/Message.java
@@ -4,16 +4,16 @@ Student - obligatoriu, Studentul care a transmis mesajul
 Timp - data si timpul, obligatoriu. Timpul exact la care a fost transmis cu precizie cat mai mare (e.g. milisecunde). Precizia este necesara pentru ordonare cronologica.
 Text - text, obligatoriu. textul mesajului
 Grupa - obligatoriu, grupa unde a fost transmis mesajul
-
+Eve, Geta, Andrei
         */
 
 import java.util.Date;
 
 public class Message{
-    private Student student = new Student();
+    //private Student student = new Student();
     private Date time ;
     private String message = "";
-    private Group group = new Group();
+    //private Group group = new Group();
 
     public Message(Date time, String message) {
         this.time = time;

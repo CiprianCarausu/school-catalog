a6728687c0d7fe044ce3e8217525f650f9ecf7f9
mirannaalina <v_mxa@yahoo.com>
2019-09-05 19:49:12 +0300

    Courses (name changed)

--- src/main/java/model/Courses.java
+++ src/main/java/model/Courses.java
@@ -1,12 +1,12 @@
 package model;
 
-public class Materie {
+public class Courses {
     String nume;
     String prescurtare;
     String detalii;
     Profesor profesori;
 
-    public Materie(String nume, String prescurtare, String detalii, Profesor profesori){
+    public Courses(String nume, String prescurtare, String detalii, Profesor profesori){
         this.nume = nume;
         this.prescurtare = prescurtare;
         this.detalii = detalii;

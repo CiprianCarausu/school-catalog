68c7747074429806e3f5f1186e42fa0a6b2d4dbb
LaurentiuNabarogu <laurentiu.nabarogu@gmail.com>
2019-09-06 13:50:38 +0300

    revised costrutor and fields, adding setters

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -1,30 +1,37 @@
 package model;
 
 public class Group {
-    private int IdGroup;     //- numeric,obligatoriu intre 1 si 10.
-    private int Year;        //obligatoriu intre 1 si 6. Reprezinta anul din care face parte grupa.
+    private int idGroup;     //- numeric,obligatoriu intre 1 si 10.
+    private int year;        //obligatoriu intre 1 si 6. Reprezinta anul din care face parte grupa.
 
-    //Studenti - lista de studenti
-    //Mesaje //- lista de mesaje asociate grupei
+    //studenti - lista de studenti
+    //mesaje //- lista de mesaje asociate grupei
+
 
     public Group(int idGroup, int year) {
-        IdGroup = idGroup;
-        Year = year;
+        if(idGroup < 1 || idGroup > 10){
+            throw new IllegalArgumentException("The group is doesn't exist.");
+        }
+        if(year < 1 || year > 6){
+            throw new IllegalArgumentException("Year not in range 1-6.");
+        }
+        this.idGroup = idGroup;
+        this.year = year;
     }
 
     public int getIdGroup() {
-        return IdGroup;
-    }
-
-    public void setIdGroup(int idGroup) {
-        IdGroup = idGroup;
+        return idGroup;
     }
 
     public int getYear() {
-        return Year;
+        return year;
+    }
+
+    public void setIdGroup(int idGroup) {
+        this.idGroup = idGroup;
     }
 
     public void setYear(int year) {
-        Year = year;
+        this.year = year;
     }
 }

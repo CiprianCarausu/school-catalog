474993d573cf0c4398e2aab70e713e0c908a6341
mirannaalina <v_mxa@yahoo.com>
2019-09-07 19:03:55 +0300

    Courses (name changed)

--- src/main/java/model/Courses.java
+++ src/main/java/model/Courses.java
@@ -1,16 +1,33 @@
 package model;
 
+/*
+Materie
+	Denumire - text, obligatoriu
+	Prescurtare - text, obligatoriu. Denumirea scurta a materiei
+	Detalii - text, obligatoriu, maxim 100 caractere. Detalii despre continutul cursului / seminarului
+	Profesori - lista de profesori care pot preda materia curenta
+
+	Courses
+    - fara modificatori de acces pe variabile
+    - lipsesc gettere si settere pt fielduri
+    - inconsistenta la Profesor, e declarata variabila ‘profesori’ asociata unui singur profesor
+
+
+ */
+
 public class Courses {
 
     private String name;
     private String shortcut;
     private String details;
-    private String teachers;
+    private String teachers;//get teachers list from Profesor's getter
 
-    public Courses(String nume, String shortcut, String details, String teachers){
+    public Courses(String name, String shortcut, String details, String teachers){
+
+
         this.name = name;
         this.shortcut = shortcut;
-        this.details = details;
+        this.details = checks(details);
         this.teachers = teachers;
 
     }
@@ -24,6 +41,14 @@ public class Courses {
     }
 
     public String getDetails() {
+        return details;
+    }
+
+    public String checks(String details){
+
+        if(details.length() >100){
+            System.out.println("ERROR. Too many details!!!");
+        }
         return details;
     }
 }

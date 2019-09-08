bde97681f5062869452f5ecb4f4b6854f48b50b2
LaurentiuNabarogu <laurentiu.nabarogu@gmail.com>
2019-09-08 19:38:52 +0300

    adding student and message fields and revised the constructor

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -3,20 +3,22 @@ package model;
 public class Group {
     private int idGroup;     //- numeric,obligatoriu intre 1 si 10.
     private int year;        //obligatoriu intre 1 si 6. Reprezinta anul din care face parte grupa.
+    Student student;        //studenti - lista de studenti
+    Message message;        //mesaje //- lista de mesaje asociate grupei
 
-    //studenti - lista de studenti
-    //mesaje //- lista de mesaje asociate grupei
+    public Group(int idGroup, int year, Student student, Message message) {
 
-
-    public Group(int idGroup, int year) {
         if(idGroup < 1 || idGroup > 10){
             throw new IllegalArgumentException("The group is doesn't exist.");
         }
         if(year < 1 || year > 6){
             throw new IllegalArgumentException("Year not in range 1-6.");
         }
+
         this.idGroup = idGroup;
         this.year = year;
+        this.student = student;
+        this.message = message;
     }
 
     public int getIdGroup() {

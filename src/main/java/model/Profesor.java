6b23ba28f54f83e3d54adebeba8a3803c2a37143
Ciprian Carausu <carausuciprian@gmail.com>
2019-09-10 13:55:17 +0300

    Added text validations. Titles class modifications.

--- src/main/java/model/Profesor.java
+++ src/main/java/model/Profesor.java
@@ -1,13 +1,18 @@
 package model;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Profesor {
     private String lastName;
     private String firstName;
     private String email;
-    private String Title;
-    private int phoneNumber;
+    private Titles title;
+    private String phoneNumber;
     private int yearOfEmployment;
     private boolean active;
+
+    public Profesor() {
+    }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
@@ -19,21 +24,79 @@ public class Profesor {
 
     public void setEmail(String email) {
         this.email = email;
+        String emailRegex = "[_a-zA-Z1-9]+(\\.[A-Za-z0-9]*)*@[A-Za-z0-9]+\\.[A-Za-z0-9]+(\\.[A-Za-z0-9]*)*";
+        Pattern emailPat = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = emailPat.matcher(email);
+
+        if (matcher.find() == false)
+            System.out.println("The email address you entered is invalid!");
     }
 
     public void setTitle(Titles title) {
-        title = title;
+        this.title = title;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+        String phoneNumberRegex = "^(\\+\\d{1,2}\\s?)?1?\\-?\\.?\\s?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$";
+        Pattern phoneNumberPat = Pattern.compile(phoneNumberRegex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = phoneNumberPat.matcher(phoneNumber);
+
+        if (matcher.find() == false)
+            System.out.println("The phone number you entered is invalid!");
     }
 
     public void setYearOfEmployment(int yearOfEmployment) {
         this.yearOfEmployment = yearOfEmployment;
+
+        if (yearOfEmployment < 1970) {
+            System.out.println("The mentioned year is below 1970.");
+        }
     }
 
     public void setActive(boolean active) {
         this.active = active;
     }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Titles getTitle() {
+        return title;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getYearOfEmployment() {
+        return yearOfEmployment;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", Title='" + title + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", yearOfEmployment=" + yearOfEmployment +
+                ", active=" + active +
+                '}';
+    }
 }
+

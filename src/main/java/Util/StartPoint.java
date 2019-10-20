7848eae83381fc51245a87dcd6cda36557723608
Ciprian Carausu <carausuciprian@gmail.com>
2019-10-20 20:49:07 +0300

    Locations ordered by capacity.

--- src/main/java/Util/StartPoint.java
+++ src/main/java/Util/StartPoint.java
@@ -8,26 +8,20 @@ public class StartPoint {
         School school = new School();
         school.startEverythingUp();
 
-        System.out.println("\n\nExercise 4");
         System.out.println("The total number of students is: " + school.countEveryStudent());
 
-        System.out.println("\n\nExercise 5");
         school.saveAllMessagesAGroup();
 
-        System.out.println("\n\nExercise 6");
         school.loadTeachers();
         System.out.println("The number of teacher who got hired past 2000 is: " + school.teachersHiredPast2000());
 
-        System.out.println("\n\nExercise 7");
-
         school.invokeResortForStudentsByBirthdate();
 
-        System.out.println("Exercise ");
         school.loadLocations();
         school.saveLocationsAvailable();
         school.saveSortedStudentsByBirthName();
         school.saveListGroupStudents();
+        school.saveSortedLocationCapacity();
     }
 }
 
-    //Save a file for each group, with the list of all the students that are in the group.
